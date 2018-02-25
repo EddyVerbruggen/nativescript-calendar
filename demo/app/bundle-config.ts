c7bb2d48a02b765cd7096b028dd27216a4c5f438
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-02-25 12:50:24 +0100

    bump deps

--- demo/app/bundle-config.ts
+++ demo/app/bundle-config.ts
@@ -1,5 +1,5 @@
 if ((<any>global).TNS_WEBPACK) {
-    require("tns-core-modules/bundle-entry-points");
+  require("tns-core-modules/bundle-entry-points");
 
-    global.registerModule("main-page", () => require("./main-page"));
+  global.registerModule("main-page", () => require("./main-page"));
 }
